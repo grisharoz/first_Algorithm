b65b2656328e8d0653221288a3dd6744de75a686
Rozentul <@grro5285>
2025-12-28 10:37:23 -0600

    quick_sorting_algorithm.py

--- quick_sorting/quick_sorting_algorithm.py
+++ quick_sorting/quick_sorting_algorithm.py
@@ -6,5 +6,4 @@ def quick_sort(array):
     less=[i for i in array[1:] if i<=pivot]
     greater=[i for i in array[1:] if i>pivot]
     return quick_sort(less) + [pivot] + quick_sort(greater)
-
 print(quick_sort([2,8,4,1,5,7,8,5,7,0]))
