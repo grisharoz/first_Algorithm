da38fe3b65daab1c2bf7a15db21b62b54a03430e
Rozentul <@grro5285>
2026-01-06 16:09:30 -0600

    BFS algorithm

--- BFS/bfs_initialization.py
+++ BFS/bfs_initialization.py
@@ -1,14 +1,32 @@
 from collections import deque
-graph={}
-graph['you']=['Alice','Bob','Carol']
-graph['Alice']=['Mike','Tom']
-graph['Bob']=['Tobias','Lukas']
-graph['Carol']=['Rob']
-graph['Mike']=[]
-graph['Tom']=[]
-graph['Tobias']=[]
-graph['Lukas']=[]
-graph['Rob']=[]
+family_graph={} #creating graph
+family_graph['Grisha']=['Andrey','Liana','Masha']
+family_graph['Andrey']=['Eva','Peter',"Marina"]
+family_graph['Liana']=["Max"]
+family_graph['Masha']=[]
+family_graph['Marina']=[]
+family_graph['Eva']=[]
+family_graph['Peter']=[]
+family_graph['Max']=[]
 
-search_queue=deque() #create neu queue
-search_queue+=graph['you'] #all neighbours include in the search
+#define which person is selling mango
+def person_is_seller(name):
+    return name[0]=="M"
+
+#function for searching seller
+def search(name):
+    search_queue=deque()
+    search_queue +=family_graph[name]
+    searched=set() #for already checked people
+    while search_queue:
+        person=search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print(person+' is selling mango')
+                return True
+            else:
+                search_queue+=family_graph[person]
+                searched.add(person)
+    return False
+
+search('Grisha')
